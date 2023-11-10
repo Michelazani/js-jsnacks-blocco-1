aa607570130c0327a44d8761f1230323cd96c298
Michelazani <michi.zani220@gmail.com>
2023-11-10 11:53:05 +0100

    inserimento due prompt

---
+++ snack-1/js/script.js
@@ -0,0 +1,10 @@
+// chiedere all'utente con due prompt gli anni di due persone e comunicare quale delle due è più grande
+
+cons typeAgeOne = parseInt (prompt ('How old are you?'));
+
+cons typeAgeTwo =  parseInt (prompt ('How old are you?'));
+
+
+if ( typeAgeOne.Math-max > typeAgeTwo.Math-max) {
+
+}
