562618277102feb441c7aa998b8c0a90d5d41b1b
Michelazani <michi.zani220@gmail.com>
2023-11-18 15:31:39 +0100

    correazione snack 10

--- jsnack-10/js/script.js
+++ jsnack-10/js/script.js
@@ -5,8 +5,13 @@
 const typeNumberOne = parseInt (prompt ('type a 2 digit number'));
 const typeNumberTwo = parseInt (prompt ('type a min 4 digit number'));
 
+let finalNumber;
 
-for (let i = typeNumberOne; i >  )
-if (typeNumberOne * 2 > typeNumberTwo){
-    console.log ('Congrats')
+
+for (let i = typeNumberOne; i <= typeNumberTwo; i=i*2){
+    if (i>=typeNumberTwo / 2){
+        finalNumber=i;
+    }
 }
+
+console.log (finalNumber*2)
